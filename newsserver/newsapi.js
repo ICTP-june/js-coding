21e44d1055532975b54b24a9a799baf980737b72
jaehunjeong <keepthefaith00@gmail.com>
2024-07-03 17:42:41 +0900

    axios 로 openapi 요청하여 데이터 html에 뿌리기

--- newsserver/newsapi.js
+++ newsserver/newsapi.js
@@ -1,36 +1,51 @@
 const express = require('express');
 const app = express();
+const axios = require('axios').default;
 
-app.get('/', (request, response) => {
-    const data = [
-        {
-            title: 'title111',
-            author: 'author111',
-            publishedAt: '2024-07-03 15:56:33',
-            url: 'http://www.naver.com'
-        },
-        {
-            title: 'title222',
-            author: 'author222',
-            publishedAt: '2024-07-03 17:58:33',
-            url: 'http://www.daum.com'
-        },
-        {
-            title: 'title333',
-            author: 'author3333',
-            publishedAt: '2024-07-01 11:56:33',
-            url: 'http://www.kakao.com'
-        },
-    ]
+const API_KEY = 'YOUR_API_KEY';
+const getApiData = async () => {
+    return await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`)
+                    .then(function(response) {
+                        console.log(response.data.articles);
+                        return response.data.articles;
+                    });
+}
+
+app.get('/', async (request, response) => {
+    // const data = [
+    //     {
+    //         title: 'title111',
+    //         author: 'author111',
+    //         publishedAt: '2024-07-03 15:56:33',
+    //         url: 'http://www.naver.com'
+    //     },
+    //     {
+    //         title: 'title222',
+    //         author: 'author222',
+    //         publishedAt: '2024-07-03 17:58:33',
+    //         url: 'http://www.daum.com'
+    //     },
+    //     {
+    //         title: 'title333',
+    //         author: 'author3333',
+    //         publishedAt: '2024-07-01 11:56:33',
+    //         url: 'http://www.kakao.com'
+    //     },
+    // ]
+    const data = await getApiData();
+    let html = '';
+    data.forEach((d) => {
+        html += `<p>${d.title}</p>
+                <p>${d.author}</p>
+                <p>${d.publishedAt}</p>
+                <a href=${d.url}>${d.url}</a>
+                <hr/>`;
+    });
     response.send(`
         <html>
         <body>
           <div id='wrapper'>
-          <p>뉴스데이터 제목(title)</p>
-          <p>뉴스데이터 작성자(author)</p>
-          <p>뉴스데이터 작성일(publishedAt)</p>
-          <a href='url'>뉴스데이터url(url)</a>
-          <hr/>
+          ${html}
           </div>
         </body>
         </html>
