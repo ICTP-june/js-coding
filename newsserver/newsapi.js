247cb6c3f898a6ca8c88b0be960c53c6e092b327
jaehunjeong <keepthefaith00@gmail.com>
2024-07-04 16:30:35 +0900

    - api urlToImage url / 붙은것 제거 후 view img태그 넣어주기 - img 없을 경우 대체 텍스트 보여주도록

--- newsserver/newsapi.js
+++ newsserver/newsapi.js
@@ -16,46 +16,20 @@ app.set('views', './views');
 
 app.get('/', async (req, res) => {
     const data = await getApiData();
+    const result = data.map((object) => {
+        if(object.urlToImage !== null && object.urlToImage.endsWith('/')) {
+            object.urlToImage = object.urlToImage.slice(0, -1);
+            return object;
+        } else {
+            return object;
+        }
+    });
     res.render('index', {
         one: "this is one",
         two: "this is two",
-        data: data
+        data: result
     });
 });
-// app.get('/', async (request, response) => {
-//     // const data = [
-//     //     {
-//     //         title: 'title111',
-//     //         author: 'author111',
-//     //         publishedAt: '2024-07-03 15:56:33',
-//     //         url: 'http://www.naver.com'
-//     //     },
-//     //     {
-//     //         title: 'title222',
-//     //         author: 'author222',
-//     //         publishedAt: '2024-07-03 17:58:33',
-//     //         url: 'http://www.daum.com'
-//     //     },
-//     // ]
-//     const data = await getApiData();
-//     let html = '';
-//     data.forEach((d) => {
-//         html += `<p>${d.title}</p>
-//                 <p>${d.author}</p>
-//                 <p>${d.publishedAt}</p>
-//                 <a href=${d.url}>${d.url}</a>
-//                 <hr/>`;
-//     });
-//     response.send(`
-//         <html>
-//         <body>
-//           <div id='wrapper'>
-//           ${html}
-//           </div>
-//         </body>
-//         </html>
-//     `);
-// });
 
 const port = 8000;
 app.listen(port, () => {
