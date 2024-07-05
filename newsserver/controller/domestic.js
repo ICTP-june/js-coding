f949d67f3d62e364228034c8084fdb16a61b054a
jaehunjeong <keepthefaith00@gmail.com>
2024-07-05 16:49:15 +0900

    불필요 코드 삭제

--- newsserver/controller/domestic.js
+++ newsserver/controller/domestic.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios').default;
 
-const testService = require('../service/test');
 const domesticService = require('../service/domesticService');
-
-const API_KEY = '380ce161b7e444158a7e387fec0419d0';
-const getApiData = async (countryCode, category = 'business') => {
-    return await axios.get(`https://newsapi.org/v2/top-headlines?country=${countryCode}&category=${category}&apiKey=${API_KEY}`)
-                    .then(function(response) {
-                        // console.log(response.data.articles);
-                        return response.data.articles;
-                    });
-}
 
 router.get('/', async (req, res, next) => {
     const result = await domesticService.getApiData('kr');
-    testService.getLog('hihihihihihihihihiih');
     res.render('domestic', {
         one: "this is one",
         two: "this is two",
@@ -25,7 +13,7 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/sports', async (req, res, next) => {
-    const data = await getApiData('kr', 'sports');
+    const data = await domesticService.getApiData('kr','sports');
     res.render('domestic-sports', {
         one: "this is one",
         two: "this is two",
